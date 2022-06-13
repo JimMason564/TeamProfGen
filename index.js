cbc1930658b3967e5b95dd10dd9197d123ef3d77
Jim Mason <jimmason564@yahoo.com>
2022-06-12 17:12:05 -0700

    Created constructors for roles

---
+++ index.js
@@ -0,0 +1,7 @@
+const inquirer = require('inquirer');
+const fs = require('js')
+const generateHTML = require("./generateHTML");
+
+//init function to gather data
+
+//function that feeds data to HTML to generate correct blocks
