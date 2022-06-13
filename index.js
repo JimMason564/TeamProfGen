d7bde3f4c457cd3ac0b9de2ed0dddcfe92094bd7
Jim Mason <jimmason564@yahoo.com>
2022-06-12 19:22:04 -0700

    Added simple functions for questions

--- index.js
+++ index.js
@@ -1,7 +1,88 @@
+
 const inquirer = require('inquirer');
-const fs = require('js')
+const fs = require('fs')
 const generateHTML = require("./generateHTML");
 
-//init function to gather data
+const Manager = require('./lib.Manager.js')
+const Engineer = require('./lib.Engineer.js')
+const Intern = require('./lib.Intern.js')
+//init function to start everything
 
-//function that feeds data to HTML to generate correct blocks
+//menu with prompts for different roles
+function managerEl () {
+  inquirer.prompt ([
+{
+  type: "input",
+  name: "managerName",
+  message: "What is this manager's name?"
+},
+{
+  type: "input",
+  name: "managerID",
+  message: "What is this manager's employee ID?" 
+},
+{
+  type: "input",
+  name: "managerEmail",
+  message: "What is this manager's email address?"
+},
+{
+  type: "input",
+  name: "managerOfficenumber",
+  message: "What is this manager's office number"
+}
+  ])
+}
+
+function engineerEl() {
+  inquirer.prompt ([
+    {
+      type: "input",
+      name: "engineerName",
+      message: "What is this engineer's name?"
+    },
+    {
+      type: "input",
+      name: "engineerID",
+      message: "What is this engineer's employee ID?" 
+    },
+    {
+      type: "input",
+      name: "engineerEmail",
+      message: "What is this engineer's email address?"
+    },
+    {
+      type: "input",
+      name: "engineererGithub",
+      message: `Please enter a link to ${this.engineerName}'s GitHub Account`
+    }
+      ])
+}
+
+function internEl() {
+  inquirer.prompt ([
+    {
+      type: "input",
+      name: "internName",
+      message: "What is this intern's name?"
+    },
+    {
+      type: "input",
+      name: "internID",
+      message: "What is this intern's employee ID?" 
+    },
+    {
+      type: "input",
+      name: "internEmail",
+      message: "What is this intern's email address?"
+    },
+    {
+      type: "input",
+      name: "internerOfficenumber",
+      message: `What school is ${this.internName} currently attending?`
+    }
+      ])
+}
+//questions for different roles
+
+//print to html
